39b9233fc95525c482a080811d0561f50ac98077
Paul Kondratyev <pavel.kondratyev@protonmail.com>
2017-02-08 23:52:31 +0700

    dev Rango in process

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    # Construct dictionary to pass to the template engine as its context
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'picture': 'Description of picture, bla bla bla', 'name': 'Paul Kondratyev'}
+    return render(request, 'rango/about.html', context=context_dict)
