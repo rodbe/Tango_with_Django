fd03d5ded59cf64bf88ae55bd59b6eb540cf6248
Paul Kondratyev <pavel.kondratyev@protonmail.com>
2017-02-09 22:04:28 +0700

    exercies done

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,7 +7,8 @@ def index(request):
     # Construct dictionary to pass to the template engine as its context
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': pages_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
